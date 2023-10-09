7efddb55d72c958d523a00b2d1ff1e6978720aa2
ShadoKiller <68161945+shadokiller@users.noreply.github.com>
2023-10-09 17:07:24 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,281 @@
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <link rel="icon" type="image/png"
+    href="https://cdn.discordapp.com/attachments/935237846019751946/987726621446402198/z69dFPkzS0dwPkXJ0FFWdUg6lk4kI4D6-removebg-preview.png">
+  <link rel="stylesheet" href="css/index.css">
+  <link rel="stylesheet" href="css/simple-line-icons.css">
+  <link rel="stylesheet" href="css/aarda.css">
+
+  <title>SHADO KİLLER</title>
+  <style>
+    body {
+  background-color: black;
+}
+    .btn-gradient-1 {
+      font: 300 28px "Helvetica Neue", sans-serif;
+      text-decoration: none;
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      background-image: linear-gradient(to right, red, yellow 50%, gray 50%);
+      background-size: 200% 100%;
+      background-position: 100%;
+      color: blue;
+      background-color: #101214;
+      border-color: #101214;
+      border: 0px;
+      font-size: 18px;
+    }
+  </style>  
+</head>
+
+<body>
+  <svg id="clip-paths" width="0" height="0" version="1.1" xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink">
+
+    <g>
+    </g>
+  </svg>
+
+
+  <div id="page">
+    <header>
+      <div id="header-content">
+        <div id="header-logo">
+          <svg id="logo" width="80" height="67" viewBox="0 0 160 134" xmlns="http://www.w3.org/2000/svg">
+          </svg>
+        </div>
+        <p style="text-align: center;"><br />
+          <span style="font-family:Tahoma,Geneva,sans-serif;"><strong><span style="font-size:5px;"><span
+                  style="color:#3498db;"><img alt="" src="https://i.hizliresim.com/bmipwci.jpg"
+                    style="width:120px;height: 120px" </div>
+                  <br>
+                  <br>
+                  <h1>SHADO KİLLER</h1>
+                  <p>Sosyal Medya Uzmanı</p>
+                  <div id="contact">
+                    <a href="https://instagram.com/shadoxyedek" id="instagram"><svg viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <title id="simpleicons-instagram-icon"></title>
+                        <path
+                          d="M12 0C8.74 0 8.333.015 7.053.072 5.775.132 4.905.333 4.14.63c-.789.306-1.459.717-2.126 1.384S.935 3.35.63 4.14C.333 4.905.131 5.775.072 7.053.012 8.333 0 8.74 0 12s.015 3.667.072 4.947c.06 1.277.261 2.148.558 2.913.306.788.717 1.459 1.384 2.126.667.666 1.336 1.079 2.126 1.384.766.296 1.636.499 2.913.558C8.333 23.988 8.74 24 12 24s3.667-.015 4.947-.072c1.277-.06 2.148-.262 2.913-.558.788-.306 1.459-.718 2.126-1.384.666-.667 1.079-1.335 1.384-2.126.296-.765.499-1.636.558-2.913.06-1.28.072-1.687.072-4.947s-.015-3.667-.072-4.947c-.06-1.277-.262-2.149-.558-2.913-.306-.789-.718-1.459-1.384-2.126C21.319 1.347 20.651.935 19.86.63c-.765-.297-1.636-.499-2.913-.558C15.667.012 15.26 0 12 0zm0 2.16c3.203 0 3.585.016 4.85.071 1.17.055 1.805.249 2.227.415.562.217.96.477 1.382.896.419.42.679.819.896 1.381.164.422.36 1.057.413 2.227.057 1.266.07 1.646.07 4.85s-.015 3.585-.074 4.85c-.061 1.17-.256 1.805-.421 2.227-.224.562-.479.96-.899 1.382-.419.419-.824.679-1.38.896-.42.164-1.065.36-2.235.413-1.274.057-1.649.07-4.859.07-3.211 0-3.586-.015-4.859-.074-1.171-.061-1.816-.256-2.236-.421-.569-.224-.96-.479-1.379-.899-.421-.419-.69-.824-.9-1.38-.165-.42-.359-1.065-.42-2.235-.045-1.26-.061-1.649-.061-4.844 0-3.196.016-3.586.061-4.861.061-1.17.255-1.814.42-2.234.21-.57.479-.96.9-1.381.419-.419.81-.689 1.379-.898.42-.166 1.051-.361 2.221-.421 1.275-.045 1.65-.06 4.859-.06l.045.03zm0 3.678c-3.405 0-6.162 2.76-6.162 6.162 0 3.405 2.76 6.162 6.162 6.162 3.405 0 6.162-2.76 6.162-6.162 0-3.405-2.76-6.162-6.162-6.162zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm7.846-10.405c0 .795-.646 1.44-1.44 1.44-.795 0-1.44-.646-1.44-1.44 0-.794.646-1.439 1.44-1.439.793-.001 1.44.645 1.44 1.439z" />
+                      </svg></a>
+                    <a href="https://t.me/shadokiller" id="instagram"><svg viewBox="0 0 24 24" <svg
+                        xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"
+                        focusable="false" width="1em" height="1em"
+                        style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);"
+                        preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                        <path
+                          d="M11.944 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12a12 12 0 0 0 12-12A12 12 0 0 0 12 0a12 12 0 0 0-.056 0zm4.962 7.224c.1-.002.321.023.465.14a.506.506 0 0 1 .171.325c.016.093.036.306.02.472c-.18 1.898-.962 6.502-1.36 8.627c-.168.9-.499 1.201-.82 1.23c-.696.065-1.225-.46-1.9-.902c-1.056-.693-1.653-1.124-2.678-1.8c-1.185-.78-.417-1.21.258-1.91c.177-.184 3.247-2.977 3.307-3.23c.007-.032.014-.15-.056-.212s-.174-.041-.249-.024c-.106.024-1.793 1.14-5.061 3.345c-.48.33-.913.49-1.302.48c-.428-.008-1.252-.241-1.865-.44c-.752-.245-1.349-.374-1.297-.789c.027-.216.325-.437.893-.663c3.498-1.524 5.83-2.529 6.998-3.014c3.332-1.386 4.025-1.627 4.476-1.635z"
+                          fill="#626262" />
+                      </svg></a>
+
+                    <a href="https://www.youtube.com/shadokilleryoutube" id="youtube"><svg role="img"
+                        viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <title>YouTube</title>
+                        <path
+                          d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
+                      </svg></a>
+                  </div>
+      </div>
+
+<br>
+      <center><p><img
+          src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fshadokiller.repl.co&count_bg=%23C83D3D&title_bg=%23057386&icon=cliqz.svg&icon_color=%23BA0808&title=Hits&edge_flat=false" />
+      </p></center>
+            <br>
+<center>
+<button class="tip-button">
+  <span class="tip-button__text">Bağış Yap</span>
+  <div class="coin-wrapper">
+    <div class="coin">
+      <div class="coin__middle"></div>
+      <div class="coin__back"></div>
+      <div class="coin__front"></div>
+    </div>
+  </div>
+</button>
+</center>
+            <br>
+            <br>
+            <br>
+    </header>
+    <div id="content">
+      <section id="articles">
+        <h2>Sosyal Medya Hesaplarım</h2>
+        <ul>
+          <li><button class="reset btn-gradient-1"
+              onclick="window.location.href = 'https://www.instagram.com/shadoxyedek';">İnstagram Hesabım</button></li>
+          <li><button class="reset btn-gradient-1"
+              onclick="window.location.href = 'https://www.t.me/shadokiller';">Telegram Hesabım</button></li>
+          <li><button class="reset btn-gradient-1"
+               onclick="window.location.href =  'https://www.t.me/shadoarsivim';">Telegram Kanalım</button></li>
+          <li><button class="reset btn-gradient-1"
+              onclick="window.location.href = 'https://www.youtube.com/channel/UC5B4nw78v_pSGn60dsnpbKg';">YouTube
+              Kanalım</button></li>
+        </ul>
+      </section>
+        <br>
+      <section id="experiments">
+        <h2>İlgi Alanlarım</h2>
+        <ul>
+          <li><button class="reset btn-gradient-1">Sosyal Medya</button></li>
+          <li><button class="reset btn-gradient-1">Cracking</button></li>
+          <li><button class="reset btn-gradient-1">Methods</button></li>
+                  <br>
+        </ul>
+        <h2>İşe Yarar Araçlar</h2>
+        <ul>
+          <li><button class="reset btn-gradient-1" onclick="window.location.href = '/hazirlogo';">Hazır Logo Yapıcı
+              </button>
+          </li>
+          <li><button class="reset btn-gradient-1"
+              onclick="window.location.href = 'https://tiktok.shadokiller.repl.co/';">Tiktok Video İndirici</button>
+          </li>
+          <li><button class="reset btn-gradient-1"
+              onclick="window.location.href = 'https://nameserver.shadokiller.repl.co/';">Nameserver Sorgulama</button>
+          </li>
+          <li><button class="reset btn-gradient-1"
+              onclick="window.location.href = 'https://shadokiller.repl.co/pepsiuretici';">Pepsi Kod Üretici</button>
+          </li>
+          <li><button class="reset btn-gradient-1" onclick="window.location.href = 'http://helloshaso.phpnet.us/?i=2?arda-dasdelen';">Hazır
+              Scriptler</button></li>
+          <li><button class="reset btn-gradient-1"
+              onclick="window.location.href = 'https://shadokiller.repl.co/ibansorgu';">
+              İban Sorgu</button></li>
+          <li><button class="reset btn-gradient-1"
+              onclick="window.location.href = 'https://shadokiller.repl.co/domainsorgu/';">
+              Domain Sorgu</button></li>
+          <li><button class="reset btn-gradient-1" onclick="window.location.href = 'https://whois.shadokiller.repl.co';">
+            
+              Whois Sorgu</button></li>
+            
+          <li><button class="reset btn-gradient-1" onclick="window.location.href = '/plakasorgu';">Plaka Sorgu </li>
+            <li><button class="reset btn-gradient-1" onclick="window.location.href = '/cennetsorgu';">Cennet Sorgu</button>
+
+              <li><button class="reset btn-gradient-1" onclick="window.location.href = '/cmsorgu';">CM Sorgu</button>
+                <li><button class="reset btn-gradient-1" onclick="window.location.href = '/ayaksorgu';">Ayak Sorgu</button>
+              </button>
+          </li>
+        </ul>
+        <br>
+        <br>
+        <h2>Eğlence Araçları</h2>
+        <ul>
+          <li><button class="reset btn-gradient-1" onclick="window.location.href = '/128milyar';">128 Milyar Dolar Nerede?</button>
+            
+          <li><button class="reset btn-gradient-1" onclick="window.location.href = '/shadosepeti';">Asgari Ücretle Geçine Bilecek Misin?</button>
+                                           
+          <li><button class="reset btn-gradient-1" onclick="window.location.href = '/google';">Kendi Google'ım</button>
+            
+            <li><button class="reset btn-gradient-1" onclick="window.location.href = '/thanos';">Thanos Beni Öldürdü Mü?</button>
+            
+             
+              
+              
+          </li>
+        </ul>
+        <br>
+        <h2>Uygulamam</h2>
+        <ul>
+          <li><button class="reset btn-gradient-1"
+              onclick="window.location.href = 'https://www.mediafire.com/file/2t466tn5z3bzwwv/Shado_Killer_v3.6.apk/file';">Tıkla
+              İndir!</button></li>
+        </ul>
+        <br>
+        <h2>Eğitim Setlerim</h2>
+        <ul>
+          <li><button class="reset btn-gradient-1"
+              onclick="window.location.href = 'https://disk.yandex.com.tr/d/B5DhIThh-Dn1DQ';">1. Arşivim</button></li>
+          <li><button class="reset btn-gradient-1"
+              onclick="window.location.href = 'https://disk.yandex.com.tr/d/lxxVmfvFW3nyPQ';">2. Arşivim</button></li>
+          <li><button class="reset btn-gradient-1"
+              onclick="window.location.href = 'https://disk.yandex.com.tr/d/bAc6ZVnZp1-86Q';">3. Arşivim</button></li>
+        </ul>
+      </section>
+      <section id="work">
+        <h2>Hakkımda</h2>
+        <ul>
+          <li>Selam, ben Shado Killer. 22 yaşındayım. Nereli olduğum veya nerede yaşadığımın pek bir önemi olduğunu düşünmüyorum; insan olmak yeterli. Yaklaşık olarak 3 yıldır bu sanal mecradayım ve edindiğim bilgiler doğrultusunda, beni takip eden değerli takipçilerime başta Instagram olmak üzere hemen hemen her alanda bir şeyler öğretiyorum. Ne kadar sanal mecrayı bırakma kararı almış olsam da bazı alışkanlıklarımdan vazgeçemiyorum işte :) Eğer sen de aramıza katılıp yeni şeyler öğrenmek istersen, yukarıda bulunan "telegram kanalım" yazısına tıkla. Daha öğrenecek çok şeyin var. Ayrıca şunu unutma: "Başarı bir yolculuktur, varış noktası değil."</li>
+        </ul>
+      </section>
+        <br>
+      <br>
+    </div>
+  </div>
+
+  <div id="intro" class="full-size">
+    <div id="logo-container" class="full-size">
+      <svg viewBox="0 0 640 536" version="1.1" xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink">
+        <path transform="scale(4 4)"
+          d="M24.44 78.157L0 53.53 53.417 0l26.39 26.612L106.38 0l53.482 53.614L136.25 77.31l-29.312-29.2-26.46 26.946-26.548-26.58-29.49 29.68zm5.335 5.375l50.033 50.415 51.108-51.286-23.775-23.684-26.712 26.72-26.372-26.404-24.28 24.24z"
+          fill="#101214" fill-rule="evenodd" />
+      </svg>
+    </div>
+    <svg id="stripes" class="full-size" width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"></svg>
+  </div>
+  <script src="js/dynamics.js"></script>
+  <script src="js/tinycolor.js"></script>
+  <script src="js/mv.es5.js"></script>
+  <script src="js/aarda.js"></script>
+  <SCRIPT LANGUAGE="Javascript">
+    document.onkeypress = function (event) {
+      event = (event || window.event);
+      if (event.keyCode == 123) {
+        //alert('No F-12');
+        return false;
+      }
+    }
+    document.onmousedown = function (event) {
+      event = (event || window.event);
+      if (event.keyCode == 123) {
+        //alert('No F-keys');
+        return false;
+      }
+    }
+    document.onkeydown = function (event) {
+      event = (event || window.event);
+      if (event.keyCode == 123) {
+        //alert('No F-keys');
+        return false;
+      }
+    }
+    var isNS = (navigator.appName == "Netscape") ? 1 : 0;
+    var EnableRightClick = 0;
+    if (isNS)
+      document.captureEvents(Event.MOUSEDOWN || Event.MOUSEUP);
+    function mischandler() {
+      if (EnableRightClick == 1) {return true;}
+      else {return false;}
+    }
+    function mousehandler(e) {
+      if (EnableRightClick == 1) {return true;}
+      var myevent = (isNS) ? e : event;
+      var eventbutton = (isNS) ? myevent.which : myevent.button;
+      if ((eventbutton == 2) || (eventbutton == 3)) return false;
+    }
+    function keyhandler(e) {
+      var myevent = (isNS) ? e : window.event;
+      if (myevent.keyCode == 96)
+        EnableRightClick = 1;
+      return;
+    }
+    document.oncontextmenu = mischandler;
+    document.onkeypress = keyhandler;
+    document.onmousedown = mousehandler;
+    document.onmouseup = mousehandler;
+//-->
+  </script>
+          </span>
+          </span>
+          </strong>
+          </span>
+        </p>
+      </div>
+    </header>
+  </div>
+</body>
+</html>
